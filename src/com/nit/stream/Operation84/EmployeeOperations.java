c17dce9276213093d69848032eaa61d715b8f88e
swapnilkature123 <swapnilkature917@gmail.com>
2025-06-06 23:27:40 +0530

    Logical

--- src/com/nit/stream/Operation84/EmployeeOperations.java
+++ src/com/nit/stream/Operation84/EmployeeOperations.java
@@ -1,5 +1,6 @@
 package com.nit.stream.Operation84;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -202,10 +203,198 @@ public class EmployeeOperations {
 			}));
 		collect.forEach( (k,v) -> System.out.println((k+" - "+v)));
 			
+		System.out.println();
+
+//		15. Find Employees with a Specific Age:
+//	    - Find all employees who are exactly 35 years old.
+		System.out.println(RED+"************ Find all employees who are exactly 35 years old.***********"+RESET);
+		list.stream()
+			.filter(age -> age.getAge()==35)
+			.forEach(System.out::println);
 		
+		System.out.println();
+		
+
+
+//		16. Calculate the Sum of Salaries by Gender:
+//		    - Calculate the sum of salaries for each gender (i.e., male and female)
+//		    and return a map with gender as the key and the sum of salaries as the value.
+		System.out.println(RED+"********* Calculate the sum of salaries for each gender**********"+RESET);
+		list.stream()
+			.collect(Collectors.groupingBy(Employee::getGender, Collectors.summingDouble(Employee::getSalary)))
+			.forEach( (k,v) -> System.out.println(k+" - "+v));
+		
+		System.out.println();
+		
+		
+
+//		17. Find Employees with Names Starting with "J":
+//		    - Find all employees whose names start with the letter "E."
+		System.out.println(RED+"*********Find all employees whose names start with the letter J******"+RESET);
+		list.stream().filter(e -> e.getName().startsWith("E"))
+			.map(Employee::getName)
+			.forEach(System.out::println);
 		
 		
 		System.out.println();
+		
+
+		//	18. Calculate the Average Salary for Male and Female Employees:
+		//		    - Calculate the average salary separately for male
+		//		    and female employees and return a map with gender
+		//		    		as the key and the average salary as the value.
+				System.out.println(RED+"**********Calculate the average salary separately for male and female*********"+RESET);
+				
+		list.stream()
+			.collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingDouble(Employee::getSalary)))
+			.forEach( (k,v) -> System.out.println(k+" - "+v));
+		
+		System.out.println();
+
+
+
+//		19. Find the Top N Highest Paid Employees:
+//		    - Find the top N employees with the highest salaries.
+		System.out.println(RED+"*******Find the top N employees with the highest salaries.********"+RESET);
+		
+//		list.stream()
+//			.sorted( (s1,s2)-> (int)(s2.getSalary()-s1.getSalary()))
+//			.limit(2)
+//			.forEach(System.out::println);
+		
+		list.stream()
+		.sorted(Comparator.comparingDouble(Employee::getSalary).reversed())
+		.limit(3)
+		.forEach(System.out::println);
+		
+		System.out.println();
+		
+
+//		20. Retrieve Distinct Age Values:
+//		    - Get a list of distinct ages of all employees.
+		System.out.println(RED+"*********Get a list of distinct ages of all employees.*********"+RESET);
+//		list.stream().filter(k->!set.add(k.getAge())).forEach(System.out::println);
+		
+		System.out.println();
+		
+		list.stream()
+			.map(Employee::getAge)
+			.distinct()
+			.forEach(System.out::println);
+		
+		System.out.println();
+		
+
+
+//		21. Find the Three Lowest-Paid Employees:
+//			   - Find and display the names of the three employees with the lowest salaries.
+		System.out.println(RED+"*******Find and display the names of the three employees with the lowest salaries.********"+RESET);
+		list.stream()
+			.sorted((s1,s2)-> (int) (s1.getSalary()-s2.getSalary()))
+			.limit(3)
+			.map(Employee::getName)
+			.forEach(System.out::println);
+		
+		
+		System.out.println();
+		
+
+//		22. Sort Employees by Name Length:
+//			   - Sort employees by the length of their names (shortest to longest).
+		System.out.println(RED+"***********Sort employees by the length of their names (shortest to longest).*******"+RESET);
+		list.stream()
+			.sorted((s1,s2)->s1.getName().length() - s2.getName().length())
+			.forEach(System.out::println);
+		
+		System.out.println();
+		
+
+//		23. Group Employees by Age Range:
+//			   - Group employees into custom
+//			   age ranges (e.g., 20-29, 30-39, etc.) and
+//			   return a map with the age range as the key and a list of employees as the value.
+		System.out.println(RED+"*********Group employees into custom age ranges (e.g., 20-29, 30-39, etc.)*********"+RESET);
+		list.stream()
+			.collect(Collectors.groupingBy((t) -> {
+										int age=(int)t.getAge()	;
+										if(age>=20 && age<30)
+											return "20-29";
+										else if(age>=30 && age<39)
+											return "30-39";
+										else 
+											return "40+";
+									}
+								)).forEach( (k,v) -> System.out.println(k+" - "+v));
+		
+		
+		System.out.println();
+		
+		
+
+//		24. Find the Average Salary of Employees Aged 30 or Younger:
+//			   - Calculate the average salary of employees aged 30 or younger.
+		System.out.println(RED+"********Calculate the average salary of employees aged 30 or younger.******"+RESET);
+		
+		double orElseThrow4 = list.stream()
+			.filter(age -> age.getAge() <= 30)
+			.mapToDouble( k -> k.getSalary()).average()
+			.orElseThrow();
+		System.out.println(orElseThrow);
+		
+		System.out.println();
+		
+		
+
+//		25. Find the Names of Male Employees with Salaries Over $60,000:
+//			   - Retrieve the names of male employees with salaries over $60,000.
+		System.out.println(RED+"**********Retrieve the names of male employees with salaries over $53,000.***********"+RESET);
+		list.stream()
+			.filter(sal -> sal.getGender().equals("Male") && sal.getSalary() > 55000)
+			.map(Employee::getName)
+			.forEach(System.out::println);
+		
+		System.out.println();
+		
+
+//		26. Find the Youngest Female Employee:
+//			   - Find the youngest female employee.
+		System.out.println(RED+"**********Find the youngest female employee.**********"+RESET);
+		list.stream()
+			.filter(s -> s.getGender().equals("Female"))
+			.sorted( (s1,s2)-> s1.getAge()-s2.getAge())
+			.limit(1)
+			.forEach(System.out::println);
+
+		
+		System.out.println();
+
+		
+
+//		27. Retrieve the Names of Employees in Reverse Order:
+//			   - Get a list of employee names in reverse order (from the last employee to the first).
+		System.out.println(RED+"******Get a list of employee names in reverse order*******"+RESET);
+		list.stream()
+			.sorted( (s1,s2) -> s2.getName().length()-s1.getName().length())
+			.map(Employee::getName)
+			.forEach(System.out::println);
+		
+		System.out.println();
+
+//		List<String> collects = list.stream().map(k->k.getName()).collect(Collectors.toList());
+//		Collections.reverse(collects);
+//		System.out.println(collects);
+		
+		System.out.println();
+		
+		
+
+//		28. Find the Highest Salary Among Female Employees:
+//			   - Find the highest salary among female employees.
+		System.out.println(RED+"******Find the highest salary among female employees."+RESET);
+		list.stream()
+			.sorted((s1,s2)-> s2.getGender().equalsIgnoreCase("Female") )
+		
+		
 		System.out.println();
 		System.out.println();
 		
